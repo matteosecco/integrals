715202a87861eaaf61305cd848264bfb918fddf6
matteosecco <36543260+matteosecco@users.noreply.github.com>
2021-10-05 16:30:08 +0200

    Update main.py
    Implementation of integration by parts which lacks of
    - a way of avoiding infinite recursion because of the function getting larger and larger at every loop
    - a way to cleverly choose the subpart of the function to substitute to improve efficiency and the likelihood of finding the correct answer
    
    Useful math function has been moved to another file

--- main.py
+++ main.py
@@ -1,63 +1,9 @@
-from sympy import integrate as intg
-from sympy.abc import x, y
+from sympy.abc import x
 from sympy import Symbol, Add, Pow, Mul
 from sympy import expand, log, exp, diff, solve, symbols
 from sympy import sin, cos, tan, asin, acos, atan, csc, sec, cot
 
-
-""" Useful math functions """
-
-
-def isint(n):
-    """ Checks if 'i' is an int because sympy has its own Integer implementation """
-    try:
-        int(n)
-        return True
-    except TypeError:
-        return False
-    return
-
-
-def islinear(f):
-    """ Checks if the function is in the form y=mx+q """
-    if type(f) == Symbol or isint(f):
-        return True
-    elif type(f) == Mul or type(f) == Add:
-        for e in f.args:
-            if not islinear(e):
-                return False
-        return True
-    else:
-        return False
-
-
-def getargs(f):
-    """ Returns a list of all args of the function and of its components """
-    l = [e for e in f.args]
-    for e in f.args:
-        l.extend(getargs(e))
-
-    l = list(set(l))
-    for e in l:
-        if type(e) == Symbol or type(e) == bool or isint(e):
-            l.remove(e)
-    return l
-
-
-def subst(f, p, v=x):
-    """ Function integration by substitution using 'p' which is a subpart of 'f' """
-    # f: function
-    # p: piece to substitute with dummy variable
-    # v: default variable
-    u = symbols("u")
-    p_inv = solve(p - u, v)[-1]
-
-    new_f = f / diff(p)
-    new_f = new_f.subs(x, p_inv)
-    new_f = integrate(new_f, v=u)
-    new_f = new_f.subs(u, p)
-
-    return new_f
+import mathf
 
 
 """ Main function """
@@ -67,12 +13,12 @@ def integrate(f, v=x):
     """
     Integrate the function 'f' where 'f' is any function defined in the sympy
     module.
-    The function must have a single variable called 'x'.
+    The function must have a single variable.
     Returns the integrated function if it finds a way to integrate
     (it may be wrong), and False if it doesn't.
     v: variable used to integrate (usually x)
     """
-
+    print(f"Function: {f}\t\tv: {v}")
     # expands the function to make it easier to integrate
     f = expand(f)
 
@@ -99,66 +45,34 @@ def integrate(f, v=x):
 
     """ SECOND TRY: use the elementary techniques of integration for basic
     integrals """
-    # y = a
-    if isint(f):
+    # y = a where isint(a)
+    if mathf.isint(f):
         return f * v
 
-    # there is only a symbol, which is not the one to integrate
-    # which has not be found in known_f
+    # y = a where type(a) == Symbol
     elif type(f) == Symbol:
         return f * v
 
     # y = Sum(a, b, c, ...)
     elif type(f) == Add:
-        return Add(*[integrate(e) for e in f.args])
+        return Add(*[integrate(e, v=v) for e in f.args])
 
     # y = Pow(a, b)
     elif type(f) == Pow:
 
-        # y = x**a
-        if type(f.args[0]) == Symbol and isint(f.args[1]):
+        # y = x**a where isint(a)
+        if type(f.args[0]) == Symbol and mathf.isint(f.args[1]):
             return Pow(f.args[0], f.args[1] + 1) / (f.args[1] + 1)
 
-        # y = a**x
-        elif isint(f.args[0]) and type(f.args[1]) == Symbol:
+        # y = a**x where isint(a)
+        elif mathf.isint(f.args[0]) and type(f.args[1]) == Symbol:
             return f / log(f.args[0])
-
-        # y = a**f(x) where f is linear  # TODO: to remove after substitution implementation
-        elif isint(f.args[0]) and islinear(f.args[1]):
-            # integration by substitution
-            u = f.args[1]
-            i = integrate(f.args[0]**v / diff(u))
-            return i.subs(v, u)
 
     # y = Mul(a, b, c, ...)
     elif type(f) == Mul:
-        # y = a*x
-        if isint(f.args[0]):
-            return f.args[0] * Mul(*[integrate(e) for e in f.args[1:]])
-
-    # y = exp(a) where 'a' is linear # TODO: to remove after substitution implementation
-    elif type(f) == exp and islinear(f.args[0]):
-        u = f.args[0]
-        i = integrate(exp(v) / diff(u))
-        return i.subs(v, u)
-
-    # y = sin(x) # TODO: to remove after substitution implementation
-    elif type(f) == sin:
-        u = f.args[0]
-        i = integrate(sin(v) / diff(u))
-        return i.subs(v, u)
-
-    # y = cos(x) # TODO: to remove after substitution implementation
-    elif type(f) == cos:
-        u = f.args[0]
-        i = integrate(cos(v) / diff(u))
-        return i.subs(v, u)
-
-    # y = tan(x) # TODO: to remove after substitution implementation
-    elif type(f) == tan:
-        u = f.args[0]
-        i = integrate(tan(v) / diff(u))
-        return i.subs(v, u)
+        # y = a*x where isint(a)
+        if mathf.isint(f.args[0]):
+            return f.args[0] * Mul(*[integrate(e, v=v) for e in f.args[1:]])
 
     """ THIRD TRY: known transformation within the function """
     # TODO: trigonometric identities
@@ -171,9 +85,25 @@ def integrate(f, v=x):
                      sec(v): 1/cos(v),
                      cot(v): 1/tan(v)}
 
-    """ FOURTH TRY: substitution for each subset of the function """  # TODO
-
+    """ FOURTH TRY: substitution for each subset of the function """
+    # TODO: remove the risk of infinite recursion by expanding the function more and more
+    # TODO: sort out unlikely possibilities / rank them by usefulness
     # gets the list of all possible pieces of function to substitute
-    possibilities = getargs(f)
+    possibilities = mathf.getargs(f)
+
+    # each 'p' is a subpart of 'f'
+    for p in possibilities:
+        u = symbols("u")
+        p_inv = solve(p - u, v)[-1]
+
+        # 'new_f' is a temp variable to perform integration by substitution
+        new_f = f / diff(p)
+        new_f = new_f.subs(x, p_inv)
+        new_f = integrate(new_f, v=u)
+        new_f = new_f.subs(u, p)
+
+        # in the case in which the function is not integrated, the loop goes on
+        if new_f:
+            return new_f
 
     return False
